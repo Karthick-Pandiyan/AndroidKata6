f0faf712ebdb5bf531910dff6a6ef45e7830fca7
karthickpandiyan <pandiyan289@gmail.com>
2020-01-07 14:37:50 +0530

    Created test case to test the Given Game should return current player as player 1, when game starts on first time.

--- app/src/test/java/com/kp/tictactoe/GameTest.kt
+++ app/src/test/java/com/kp/tictactoe/GameTest.kt
@@ -1,5 +1,15 @@
 package com.kp.tictactoe
 
+import com.kp.tictactoe.model.Game
+import org.junit.Assert
+import org.junit.Test
+
 class GameTest {
 
+    val game = Game("Karthick", "Pandiyan")
+
+    @Test
+    fun `Given Game should return current player as player 1, when game starts on first time`(){
+        Assert.assertEquals("Karthick", game.currentPlayer.name)
+    }
 }
