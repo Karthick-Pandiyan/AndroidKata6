75ce8c6d71821a86d6a4d492f56f2cdfb9934246
karthickpandiyan <pandiyan289@gmail.com>
2020-01-07 15:54:08 +0530

    Added 2D array of Cells to lock the values in grid for tic tac toe.

--- app/src/main/java/com/kp/tictactoe/model/Game.kt
+++ app/src/main/java/com/kp/tictactoe/model/Game.kt
@@ -2,9 +2,15 @@ package com.kp.tictactoe.model
 
 class Game(playerOne: String, playerTwo: String){
 
+    private val BOARD_SIZE = 3
     val player1 =  Player(playerOne, "x")
     val player2 = Player(playerTwo, "o")
     var currentPlayer = player1
+    var cells = Array(BOARD_SIZE) {
+        Array(BOARD_SIZE) {
+            Cell(null)
+        }
+    }
 
     fun switchPlayer() {
         currentPlayer = if (currentPlayer === player1) player2 else player1
