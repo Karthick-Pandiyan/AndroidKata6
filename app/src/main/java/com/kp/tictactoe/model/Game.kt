f791edc471dad8bfe36de1caefbe1b8dce39e94a
karthickpandiyan <pandiyan289@gmail.com>
2020-01-07 17:51:04 +0530

    Add condition in areEquals function, make it return false when base player value is not equal to players retrieved from list of cells.

--- app/src/main/java/com/kp/tictactoe/model/Game.kt
+++ app/src/main/java/com/kp/tictactoe/model/Game.kt
@@ -45,6 +45,11 @@ class Game(playerOne: String, playerTwo: String){
             if (cell == null || cell.player?.value.isNullOrEmpty())
                 return false
 
+        val comparisonBase = cells[0]
+        for (i in 1 until cells.size)
+            if (!comparisonBase.player?.value.equals(cells[i].player?.value))
+                return false
+
         return true
     }
 }
