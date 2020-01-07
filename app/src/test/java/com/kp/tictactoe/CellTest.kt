900714771ba34fadcec8303e16cb4f2df16281f3
karthickpandiyan <pandiyan289@gmail.com>
2020-01-07 15:14:33 +0530

    Created test case to test Given function should return not empty when player started the game.

--- app/src/test/java/com/kp/tictactoe/CellTest.kt
+++ app/src/test/java/com/kp/tictactoe/CellTest.kt
@@ -1,5 +1,16 @@
 package com.kp.tictactoe
 
-class CellTest {
+import com.kp.tictactoe.model.Cell
+import com.kp.tictactoe.model.Game
+import org.junit.Assert
+import org.junit.Test
 
+class CellTest {
+    val game = Game("Karthick", "Pandiyan")
+
+    @Test
+    fun `Given function should return not empty when player started the game`(){
+        val cell = Cell(game.player1)
+        Assert.assertFalse(cell.isEmpty)
+    }
 }
