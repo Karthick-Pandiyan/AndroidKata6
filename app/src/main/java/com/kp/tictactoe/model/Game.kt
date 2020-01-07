6e72d353a736f78a461dadd8bd916dc33140a12a
karthickpandiyan <pandiyan289@gmail.com>
2020-01-07 14:37:14 +0530

    Refactored Game class to accept player names with help of Player class.

--- app/src/main/java/com/kp/tictactoe/model/Game.kt
+++ app/src/main/java/com/kp/tictactoe/model/Game.kt
@@ -1,5 +1,8 @@
 package com.kp.tictactoe.model
 
-class Game(var playerOne: String, var PlayerTwo: String){
+class Game(playerOne: String, playerTwo: String){
 
+    private val player1 =  Player(playerOne, "x")
+    private val player2 = Player(playerTwo, "o")
+    val currentPlayer = player1
 }
