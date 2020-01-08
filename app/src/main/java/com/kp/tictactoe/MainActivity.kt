b6da6e6c68cb93afee6f12c2028e09cc0a0cbd05
karthickpandiyan <pandiyan289@gmail.com>
2020-01-08 15:45:08 +0530

    Initialise DataBinding for the layout which is used to show TicTacToe.

--- app/src/main/java/com/kp/tictactoe/MainActivity.kt
+++ app/src/main/java/com/kp/tictactoe/MainActivity.kt
@@ -1,18 +1,21 @@
 package com.kp.tictactoe
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import androidx.databinding.DataBindingUtil
+import com.kp.tictactoe.databinding.ActivityMainBinding
+import com.kp.tictactoe.viewmodel.GameViewModel
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var activityGameBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        activityGameBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
