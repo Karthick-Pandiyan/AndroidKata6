3c267dc39b2974c93c25ad3fd2758b3a0aa7e287
karthickpandiyan <pandiyan289@gmail.com>
2020-01-08 10:40:53 +0530

    Created init function to accept players to play game.

--- app/src/test/java/com/kp/tictactoe/GameViewModelTest.kt
+++ app/src/test/java/com/kp/tictactoe/GameViewModelTest.kt
@@ -1,5 +1,15 @@
 package com.kp.tictactoe
 
+import androidx.databinding.ObservableArrayMap
+import com.kp.tictactoe.model.Game
+
 class GameViewModelTest {
 
+    lateinit var cells: ObservableArrayMap<String, String>
+    private var game: Game? = null
+
+    private fun init(player1: String, player2: String) {
+        game = Game(player1, player2)
+        cells = ObservableArrayMap()
+    }
 }
