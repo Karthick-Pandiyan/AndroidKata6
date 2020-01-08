fdea9bf1025f8d101de2903c6b3dc0f3d9042126
karthickpandiyan <pandiyan289@gmail.com>
2020-01-08 12:01:37 +0530

    Refactored GameViewModel class and perform all the actions which is required to listen and play tic tac toe game. And Integrated it with GameViewModelTest.

--- app/src/main/java/com/kp/tictactoe/viewmodel/GameViewModel.kt
+++ app/src/main/java/com/kp/tictactoe/viewmodel/GameViewModel.kt
@@ -1,5 +1,27 @@
 package com.kp.tictactoe.viewmodel
 
+import androidx.databinding.ObservableArrayMap
+import com.kp.tictactoe.model.Cell
+import com.kp.tictactoe.model.Game
+import com.kp.tictactoe.utilities.StringUtility.Companion.stringFromNumbers
+
 class GameViewModel {
 
+
+    lateinit var cells: ObservableArrayMap<String, String>
+    lateinit var game: Game
+
+    fun init(player1: String, player2: String) {
+        game = Game(player1, player2)
+        cells = ObservableArrayMap()
+    }
+
+    fun onClickedCellAt(row: Int, column: Int) {
+        if (game.cells[row][column].isEmpty) {
+            game.cells[row][column] = Cell(game.currentPlayer)
+            cells[stringFromNumbers(row, column)] = game.currentPlayer.value
+            if (!game.hasGameEnded())
+                game.switchPlayer()
+        }
+    }
 }
--- app/src/test/java/com/kp/tictactoe/GameViewModelTest.kt
+++ app/src/test/java/com/kp/tictactoe/GameViewModelTest.kt
@@ -1,48 +1,24 @@
 package com.kp.tictactoe
 
-import androidx.databinding.ObservableArrayMap
-import com.kp.tictactoe.model.Cell
-import com.kp.tictactoe.model.Game
+import com.kp.tictactoe.utilities.StringUtility.Companion.stringFromNumbers
+import com.kp.tictactoe.viewmodel.GameViewModel
 import org.junit.Assert
 import org.junit.Test
 
 class GameViewModelTest {
 
-    lateinit var cells: ObservableArrayMap<String, String>
-    private lateinit var game: Game
-
-    private fun init(player1: String, player2: String) {
-        game = Game(player1, player2)
-        cells = ObservableArrayMap()
-    }
-
-    fun onClickedCellAt(row: Int, column: Int) {
-        if (game.cells[row][column] == null || game.cells[row][column].isEmpty) {
-            game.cells[row][column] = Cell(game.currentPlayer)
-            cells[stringFromNumbers(row, column)] = game.currentPlayer.value
-            if (!game.hasGameEnded())
-                    game.switchPlayer()
-        }
-    }
-
+    val gameViewModel = GameViewModel()
     @Test
     fun `Given function should switch player to player 2 if the game is not ended`(){
-        init("Karthick","Pandiyan")
-        onClickedCellAt(0,0)
-        Assert.assertEquals("Pandiyan", game.currentPlayer.name)
-    }
-
-    fun stringFromNumbers(vararg numbers: Int): String {
-        val sNumbers = StringBuilder()
-        for (number in numbers)
-            sNumbers.append(number)
-        return sNumbers.toString()
+        gameViewModel.init("Karthick","Pandiyan")
+        gameViewModel.onClickedCellAt(0,0)
+        Assert.assertEquals("Pandiyan", gameViewModel.game.currentPlayer.name)
     }
 
     @Test
     fun `Given function should return player 1 name when init called from GameViewModel`(){
-        init("Karthick","Pandiyan")
-        Assert.assertEquals("Karthick", game.player1.name)
+        gameViewModel.init("Karthick","Pandiyan")
+        Assert.assertEquals("Karthick", gameViewModel.game.player1.name)
     }
 
     @Test
