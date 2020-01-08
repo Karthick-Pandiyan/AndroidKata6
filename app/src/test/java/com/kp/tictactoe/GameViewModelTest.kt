d04c163af4a869a2ee3ce526af022f75964e11b3
karthickpandiyan <pandiyan289@gmail.com>
2020-01-08 10:51:47 +0530

    Created test case to test the Given function should return String of given numbers.

--- app/src/test/java/com/kp/tictactoe/GameViewModelTest.kt
+++ app/src/test/java/com/kp/tictactoe/GameViewModelTest.kt
@@ -21,9 +21,23 @@ class GameViewModelTest {
         }
     }
 
+    fun stringFromNumbers(vararg numbers: Int): String {
+        val sNumbers = StringBuilder()
+        for (number in numbers)
+            sNumbers.append(number)
+        return sNumbers.toString()
+    }
+
     @Test
     fun `Given function should return player 1 name when init called from GameViewModel`(){
         init("Karthick","Pandiyan")
         Assert.assertEquals("Karthick", game.player1.name)
+    }
+
+    @Test
+    fun `Given function should return String of given numbers`(){
+        Assert.assertEquals("01", stringFromNumbers(0,1))
+        Assert.assertEquals("02", stringFromNumbers(0,2))
+        Assert.assertEquals("22", stringFromNumbers(2,2))
     }
 }
