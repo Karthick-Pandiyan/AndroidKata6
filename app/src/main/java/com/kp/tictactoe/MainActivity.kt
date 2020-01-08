597a41110473e2da141d514499515c09e1d422ad
karthickpandiyan <pandiyan289@gmail.com>
2020-01-08 15:46:40 +0530

    Configure players in init function and map GameViewModel to DataBinding to listen its values

--- app/src/main/java/com/kp/tictactoe/MainActivity.kt
+++ app/src/main/java/com/kp/tictactoe/MainActivity.kt
@@ -11,11 +11,15 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private lateinit var activityGameBinding: ActivityMainBinding
+    private lateinit var gameViewModel: GameViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         activityGameBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        gameViewModel = GameViewModel()
+        gameViewModel.init("Player1", "Player2")
+        activityGameBinding.setGameViewModel(gameViewModel)
     }
 }
